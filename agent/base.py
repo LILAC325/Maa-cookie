a0c843ebec5819d8f650d9d9d7714d2f739fd3e6
LILAC325 <2645557315@qq.com>
2025-10-23 17:06:30 +0800

    初始功能

---
+++ agent/base.py
@@ -0,0 +1,29 @@
+import json
+
+from maa.agent.agent_server import AgentServer
+from maa.custom_action import CustomAction
+from maa.context import Context
+
+
+@AgentServer.custom_action("DisableNode")
+class DisableNode(CustomAction):
+    """
+    将特定 node 设置为 disable 状态 。
+
+    参数格式:
+    {
+        "node_name": "结点名称"
+    }
+    """
+
+    def run(
+        self,
+        context: Context,
+        argv: CustomAction.RunArg,
+    ) -> CustomAction.RunResult:
+
+        node_name = json.loads(argv.custom_action_param)["node_name"]
+
+        context.override_pipeline({f"{node_name}": {"enabled": False}})
+
+        return CustomAction.RunResult(success=True)
--- agent/main.py
+++ agent/main.py
@@ -3,18 +3,11 @@ import sys
 from maa.agent.agent_server import AgentServer
 from maa.toolkit import Toolkit
 
-import my_action
-import my_reco
-
+import base
 
 def main():
-    Toolkit.init_option("./")
+    Toolkit.init_option("032f874d-d96c-4f2a-86a7-df10dbb80de6")
 
-    if len(sys.argv) < 2:
-        print("Usage: python main.py <socket_id>")
-        print("socket_id is provided by AgentIdentifier.")
-        sys.exit(1)
-        
     socket_id = sys.argv[-1]
 
     AgentServer.start_up(socket_id)
