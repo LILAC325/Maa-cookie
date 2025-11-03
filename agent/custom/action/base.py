6ef8fb32ec477763ef7ed97796e226a10f7bcee8
LILAC325 <2645557315@qq.com>
2025-11-04 01:21:27 +0800

    NodeOverride自定义动作 增加节点可复用性

--- agent/custom/action/base.py
+++ agent/custom/action/base.py
@@ -50,3 +50,30 @@ class EnableNode(CustomAction):
         context.override_pipeline({f"{node_name}": {"enabled": True}})
 
         return CustomAction.RunResult(success=True)
+
+@AgentServer.custom_action("NodeOverride")
+class NodeOverride(CustomAction):
+    """
+    在 node 中执行 pipeline_override 。
+
+    参数格式:
+    {
+        "node_name": {"被覆盖参数": "覆盖值",...},
+        "node_name1": {"被覆盖参数": "覆盖值",...}
+    }
+    """
+
+    def run(
+        self,
+        context: Context,
+        argv: CustomAction.RunArg,
+    ) -> CustomAction.RunResult:
+
+        ppover = json.loads(argv.custom_action_param)
+
+        if not ppover:
+            return CustomAction.RunResult(success=True)
+
+        context.override_pipeline(ppover)
+
+        return CustomAction.RunResult(success=True)
